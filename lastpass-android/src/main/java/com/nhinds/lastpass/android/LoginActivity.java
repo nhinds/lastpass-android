bd75942610104cbdfbd06495d32b2f5a5f9e006d
Nicholas Hinds <hindsn@gmail.com>
2013-10-30 19:24:21 -1000

    Cleanup
    Removed commented-out code
    Removed unused code
    Reformatted code to match style settings more closely
    Removed unused resources
    Removed old copyright notices from files which have had their original content completely removed

--- lastpass-android/src/main/java/com/nhinds/lastpass/android/LoginActivity.java
+++ lastpass-android/src/main/java/com/nhinds/lastpass/android/LoginActivity.java
@@ -2,36 +2,30 @@ package com.nhinds.lastpass.android;
 
 import org.apache.commons.lang.Validate;
 
-import com.nhinds.lastpass.LastPass.PasswordStoreBuilder;
-import com.nhinds.lastpass.GoogleAuthenticatorRequired;
-import com.nhinds.lastpass.LastPassException;
-import com.nhinds.lastpass.PasswordStore;
-import com.nhinds.lastpass.impl.LastPassImpl;
-import com.sun.jersey.spi.StringReader.ValidateDefaultValue;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.text.TextUtils;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.nhinds.lastpass.GoogleAuthenticatorRequired;
+import com.nhinds.lastpass.LastPass.PasswordStoreBuilder;
+import com.nhinds.lastpass.LastPassException;
+import com.nhinds.lastpass.PasswordStore;
+import com.nhinds.lastpass.impl.LastPassImpl;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
 public class LoginActivity extends Activity {
-	
+
 	/**
 	 * Keep track of the login task to ensure we can cancel it if requested.
 	 */
@@ -55,67 +49,64 @@ public class LoginActivity extends Activity {
 		setContentView(R.layout.activity_login);
 
 		// Set up the login form.
-		mEmailView = (EditText) findViewById(R.id.email);
+		this.mEmailView = (EditText) findViewById(R.id.email);
 
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
-					}
-				});
+		this.mPasswordView = (EditText) findViewById(R.id.password);
+		this.mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+				if (id == R.id.login || id == EditorInfo.IME_NULL) {
+					attemptLogin();
+					return true;
+				}
+				return false;
+			}
+		});
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+		this.mLoginFormView = findViewById(R.id.login_form);
+		this.mLoginStatusView = findViewById(R.id.login_status);
+		this.mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
 
-		findViewById(R.id.sign_in_button).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						attemptLogin();
-					}
-				});
+		findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				attemptLogin();
+			}
+		});
 	}
 
 	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
+	 * Attempts to sign in specified by the login form. If there are form errors
+	 * (invalid email, missing fields, etc.), the errors are presented and no
+	 * actual login attempt is made.
 	 */
 	private void attemptLogin() {
-		if (mAuthTask != null) {
+		if (this.mAuthTask != null) {
 			return;
 		}
 
 		// Reset errors.
-		mEmailView.setError(null);
-		mPasswordView.setError(null);
+		this.mEmailView.setError(null);
+		this.mPasswordView.setError(null);
 
 		// Store values at the time of the login attempt.
-		mEmail = mEmailView.getText().toString();
-		mPassword = mPasswordView.getText().toString();
+		this.mEmail = this.mEmailView.getText().toString();
+		this.mPassword = this.mPasswordView.getText().toString();
 
 		boolean cancel = false;
 		View focusView = null;
 
 		// Check for a valid password.
-		if (TextUtils.isEmpty(mPassword)) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			focusView = mPasswordView;
+		if (TextUtils.isEmpty(this.mPassword)) {
+			this.mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = this.mPasswordView;
 			cancel = true;
 		}
 
 		// Check for a valid email address.
-		if (TextUtils.isEmpty(mEmail)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
+		if (TextUtils.isEmpty(this.mEmail)) {
+			this.mEmailView.setError(getString(R.string.error_field_required));
+			focusView = this.mEmailView;
 			cancel = true;
 		}
 
@@ -126,10 +117,12 @@ public class LoginActivity extends Activity {
 		} else {
 			// Show a progress spinner, and kick off a background task to
 			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			this.mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
 			showProgress(true);
-			mAuthTask = new UserLoginTask();
-			mAuthTask.execute(new LastPassImpl().getPasswordStoreBuilder(mEmail, mPassword, null));//TODO cache file...
+			this.mAuthTask = new UserLoginTask();
+			this.mAuthTask.execute(new LastPassImpl().getPasswordStoreBuilder(this.mEmail, this.mPassword, null));// TODO
+																													// cache
+																													// file...
 		}
 	}
 
@@ -137,62 +130,55 @@ public class LoginActivity extends Activity {
 	 * Shows the progress UI and hides the login form. TODO make this less crazy
 	 */
 	private void showProgress(final boolean show) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
+		int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
+		this.mLoginStatusView.setVisibility(View.VISIBLE);
+		this.mLoginStatusView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				LoginActivity.this.mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			}
+		});
 
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
+		this.mLoginFormView.setVisibility(View.VISIBLE);
+		this.mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				LoginActivity.this.mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+			}
+		});
 	}
-	
+
 	private static class LoginResult {
 		public final LoginFailureReason failureReason;
 		public final PasswordStore passwordStore;
 		public final String reasonString;
-		
+
 		public LoginResult(PasswordStore passwordStore) {
 			Validate.notNull(passwordStore);
 			this.passwordStore = passwordStore;
 			this.failureReason = null;
 			this.reasonString = null;
 		}
+
 		public LoginResult(LoginFailureReason failureReason, String reasonString) {
 			Validate.notNull(failureReason);
 			this.failureReason = failureReason;
 			this.reasonString = reasonString;
 			this.passwordStore = null;
 		}
+
 		public LoginResult(LoginFailureReason failureReason) {
-			this(failureReason,null);
+			this(failureReason, null);
 		}
 	}
-	
+
 	private enum LoginFailureReason {
-		FAIL,
-		OTP
+		FAIL, OTP
 	}
 
 	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
+	 * Represents an asynchronous login task used to authenticate the user.
 	 */
 	public class UserLoginTask extends AsyncTask<PasswordStoreBuilder, Void, LoginResult> {
 		@Override
@@ -209,24 +195,25 @@ public class LoginActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(final LoginResult loginResult) {
-			mAuthTask = null;
+			LoginActivity.this.mAuthTask = null;
 			showProgress(false);
 
 			if (loginResult.passwordStore != null) {
 				SoftKeyboard.setPasswordStore(loginResult.passwordStore);
 				finish();
 			} else if (loginResult.failureReason == LoginFailureReason.OTP) {
-				//TODO
+				android.util.Log.i(getPackageName(), "OTP Required");
+				// TODO
 			} else {
-				mPasswordView.setError(loginResult.reasonString);
-				mPasswordView.requestFocus();
+				LoginActivity.this.mPasswordView.setError(loginResult.reasonString);
+				LoginActivity.this.mPasswordView.requestFocus();
 			}
 		}
 
 		@Override
 		protected void onCancelled() {
-			//TODO who calls me?
-			mAuthTask = null;
+			// TODO who calls me?
+			LoginActivity.this.mAuthTask = null;
 			showProgress(false);
 		}
 	}
--- lastpass-android/src/main/java/com/nhinds/lastpass/android/SoftKeyboard.java
+++ lastpass-android/src/main/java/com/nhinds/lastpass/android/SoftKeyboard.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2008-2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nhinds.lastpass.android;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -27,13 +10,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.DialogInterface.OnDismissListener;
-import android.content.DialogInterface.OnShowListener;
 import android.content.Intent;
 import android.inputmethodservice.InputMethodService;
-import android.os.IBinder;
 import android.text.InputType;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
@@ -46,27 +26,8 @@ import com.google.common.collect.Collections2;
 import com.nhinds.lastpass.PasswordInfo;
 import com.nhinds.lastpass.PasswordStore;
 
-/**
- * Example of writing an input method for a soft keyboard.  This code is
- * focused on simplicity over completeness, so it should in no way be considered
- * to be a complete soft keyboard implementation.  Its purpose is to provide
- * a basic example for how you would get started writing an input method, to
- * be fleshed out as appropriate.
- */
 public class SoftKeyboard extends InputMethodService {
-    private static final int AFTER_INSERTED_TEXT = 1;
-
-	static final boolean DEBUG = false;
-    
-    /**
-     * This boolean indicates the optional example code for performing
-     * processing of hard keys in addition to regular text generation
-     * from on-screen interaction.  It would be used for input methods that
-     * perform language translations (such as converting text entered on 
-     * a QWERTY keyboard to Chinese), but may not be used for input methods
-     * that are primarily intended to be used for on-screen text entry.
-     */
-    static final boolean PROCESS_HARD_KEYS = true;
+	private static final int AFTER_INSERTED_TEXT = 1;
 
 	private static final Comparator<PasswordInfo> BY_NAME = new Comparator<PasswordInfo>() {
 
@@ -76,155 +37,104 @@ public class SoftKeyboard extends InputMethodService {
 		}
 	};
 
-    private InputMethodManager mInputMethodManager;
+	private InputMethodManager mInputMethodManager;
 
-    private View mInputView;
-    private IBinder token;
+	private View mInputView;
 
 	private static PasswordStore passwordStore;
 
 	static void setPasswordStore(PasswordStore passwordStore) {
-		// TODO This fucking sucks, why is this so difficult to accomplish in android? 
+		// TODO This sucks, why is this so difficult to accomplish in android?
 		SoftKeyboard.passwordStore = passwordStore;
 	}
-    
-    /**
-     * Main initialization of the input method component.  Be sure to call
-     * to super class.
-     */
-    @Override public void onCreate() {
-        super.onCreate();
-        mInputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-    }
-    
-    /**
-     * This is the point where you can do all of your UI initialization.  It
-     * is called after creation and any configuration change.
-     */
-    @Override public void onInitializeInterface() {
-    }
-    
-    @Override
-    public AbstractInputMethodImpl onCreateInputMethodInterface() {
-    	return new InputMethodImpl() {
-    		@Override
-    		public void attachToken(IBinder token) {
-    			super.attachToken(token);
-    			if (SoftKeyboard.this.token == null) {
-    				SoftKeyboard.this.token = token;
-    			}
-    		}
-    	};
-    }
-    
-    @Override
-    public View onCreateInputView() {
-    	// This is freaking ugly, but the only way I can see to make the code wait until its input token is valid...
-    	mInputView = new View(this);
-    	mInputView.post(new Runnable() {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		this.mInputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+	}
+
+	@Override
+	public View onCreateInputView() {
+		// This is freaking ugly, but the only way I can see to make the code
+		// wait until its input token is valid...
+		this.mInputView = new View(this);
+		this.mInputView.post(new Runnable() {
 			@Override
 			public void run() {
-//				if (getPackageName().equals(getCurrentInputEditorInfo().packageName)) {
-//					Log.i(getPackageName(), "Run away");
-//					switchToLastInputMethod();
-//				} else {
-					Log.i(getPackageName(), "Bing");
-					bing();
-//				}
+				Log.i(getPackageName(), "Bing");
+				bing();
 			}
 		});
-		return mInputView;
-    }
-    
-    @Override
-    public void onStartInputView(EditorInfo info, boolean restarting) {
-    	if (getPackageName().equals(getCurrentInputEditorInfo().packageName)) {
+		return this.mInputView;
+	}
+
+	@Override
+	public void onStartInputView(EditorInfo info, boolean restarting) {
+		if (getPackageName().equals(getCurrentInputEditorInfo().packageName)) {
 			Log.i(getPackageName(), "Run away away");
 			switchToLastInputMethod();
-    	} else {
-    		Log.i(getPackageName(), "This input seems to be fine");
-    	}
-    }
-    
-
-    /**
-     * This is the main point where we do our initialization of the input method
-     * to begin operating on an application.  At this point we have been
-     * bound to the client, and are now receiving all of the detailed information
-     * about the target of our edits.
-     * <p>
-     * {@inheritDoc}
-     */
-    @Override public void onStartInput(EditorInfo attribute, boolean restarting) {
-    }
+		} else {
+			Log.i(getPackageName(), "This input seems to be fine");
+		}
+	}
 
 	private void bing() {
 		if (passwordStore == null) {
 			startActivity(new Intent(this, LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
 		} else {
-			//		ArrayAdapter<String> itemAdaptor = new ArrayAdapter<String>(this, R.layout.apparentlyweneedlotsofxml, Arrays.asList("I","like","candy"));
 			final List<PasswordInfo> passwords = new ArrayList<PasswordInfo>(passwordStore.getPasswords());
 			Collections.sort(passwords, BY_NAME);
-			
+
 			AlertDialog dialog = new AlertDialog.Builder(this).setItems(convert(passwords), new OnClickListener() {
-				
+
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					PasswordInfo passwordInfo = passwords.get(which);
 					Toast.makeText(SoftKeyboard.this, passwordInfo.getUrl(), Toast.LENGTH_LONG).show();
 					boolean password = isPassword(getCurrentInputEditorInfo().inputType);
-					
+
 					final String text = password ? passwordInfo.getPassword() : passwordInfo.getUsername();
 					getCurrentInputConnection().commitText(text, AFTER_INSERTED_TEXT);
 				}
 			}).setOnDismissListener(new OnDismissListener() {
-				
+
 				@Override
 				public void onDismiss(DialogInterface dialog) {
 					switchToLastInputMethod();
 				}
 			}).create();
-	    	makeDialogWork(dialog);
+			makeDialogWork(dialog);
 			dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-	    	dialog.show();
+			dialog.show();
 		}
 	}
 
 	private void makeDialogWork(AlertDialog dialog) {
 		// http://stackoverflow.com/questions/5698700/how-to-launch-a-popupwindow-or-dialog-from-an-input-method-service
 		LayoutParams attributes = dialog.getWindow().getAttributes();
-		attributes.token = mInputView.getWindowToken();
-		attributes.type=WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG;
+		attributes.token = this.mInputView.getWindowToken();
+		attributes.type = WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG;
 		dialog.getWindow().setAttributes(attributes);
 	}
 
 	protected static boolean isPassword(int inputType) {
 		int variation = inputType & InputType.TYPE_MASK_VARIATION;
-		return variation == InputType.TYPE_TEXT_VARIATION_PASSWORD
-				|| variation == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+		return variation == InputType.TYPE_TEXT_VARIATION_PASSWORD || variation == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
 				|| variation == InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD;
 	}
 
-    private static String[] convert(Collection<? extends PasswordInfo> passwords) {
-		return Collections2.transform(passwords,
-				new Function<PasswordInfo, String>() {
+	private static String[] convert(Collection<? extends PasswordInfo> passwords) {
+		return Collections2.transform(passwords, new Function<PasswordInfo, String>() {
 
-					@Override
-					public String apply(PasswordInfo passwordInfo) {
-						return passwordInfo.getName();
-					}
-				}).toArray(new String[passwords.size()]);
+			@Override
+			public String apply(PasswordInfo passwordInfo) {
+				return passwordInfo.getName();
+			}
+		}).toArray(new String[passwords.size()]);
 	}
 
-
-	/**
-     * This is called when the user is done editing a field.  We can use
-     * this to reset our state.
-     */
-    @Override public void onFinishInput() {
-    }
-
-    private void switchToLastInputMethod() {
-		mInputMethodManager.switchToLastInputMethod(getWindow().getWindow().getAttributes().token);
+	private void switchToLastInputMethod() {
+		this.mInputMethodManager.switchToLastInputMethod(getWindow().getWindow().getAttributes().token);
 	}
 }
